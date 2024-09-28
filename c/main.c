354d6a392588f78f48c565f61e5566d07b4d127e
Antonin <bo.antonin@gmail.com>
2024-09-28 16:17:39 +0200

    main base cree

--- c/main.c
+++ c/main.c
@@ -1,40 +1,48 @@
-#include <SDL2/SDL.h>
-#include <SDL2_gfxPrimitives.h>
+#include <SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define WIDTH 600 
-#define HEIGHT 800
+#define WIDTH 1920
+#define HEIGHT 1080
+#define DEFAULTCOLOR_R 0
+#define DEFAULTCOLOR_G 255
+#define DEFAULTCOLOR_B 0
+#define DEFAULTCOLOR_A 255
 
 
+typedef struct PEN{
+    int size,penDown;
+    float x,y,angle;
+    SDL_Color color;
+}PEN;
 
-int main(int argc, char* argv[]) {
-    // Initialiser SDL
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+SDL_Window * initSDL(){
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Erreur d'initialisation de SDL: %s\n", SDL_GetError());
-        return 1;
+        exit(1);
     }
 
-    // Créer une fenêtre SDL
-    SDL_Window* window = SDL_CreateWindow("Fenêtre SDL2", 
-                                          SDL_WINDOWPOS_UNDEFINED, 
-                                          SDL_WINDOWPOS_UNDEFINED, 
-                                          HEIGHT, WIDTH, 
+    SDL_Window * window = SDL_CreateWindow("Fenêtre SDL2",
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          SDL_WINDOWPOS_UNDEFINED,
+                                          WIDTH, HEIGHT,
                                           SDL_WINDOW_SHOWN);
-                                          
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
     if (!window) {
         printf("Erreur de création de la fenêtre: %s\n", SDL_GetError());
         SDL_Quit();
-        return 1;
+        exit(1);
     }
 
+    return window;
+}
 
-
-
-
-
-
-
-    // Gérer l'événement de fermeture
+void closeEventSDL(SDL_Color ** matrix){
     SDL_Event e;
     int quit = 0;
     while (!quit) {
@@ -48,6 +56,54 @@ int main(int argc, char* argv[]) {
     // Nettoyer et quitter SDL
     SDL_DestroyWindow(window);
     SDL_Quit();
-    
+
+    for (int i = 0; i < WIDTH; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+SDL_Color ** init2DSpace() {
+    SDL_Color **matrix = malloc(sizeof(SDL_Color*) * WIDTH);
+    if (matrix == NULL) exit(1);
+
+    for (int i = 0; i < WIDTH; i++) {
+        matrix[i] = malloc(sizeof(SDL_Color) * HEIGHT);
+        if (matrix[i] == NULL) exit(1);
+
+        for (int e = 0; e < HEIGHT; e++) {
+            matrix[i][e].r = DEFAULTCOLOR_R;
+            matrix[i][e].g = DEFAULTCOLOR_G;
+            matrix[i][e].b = DEFAULTCOLOR_B;
+            matrix[i][e].a = DEFAULTCOLOR_A;
+        }
+    }
+
+    return matrix;
+}
+
+void RenderMatrix(SDL_Color** matrix) {
+    for (int i = 0; i < WIDTH; i++) {
+        for (int e = 0; e < HEIGHT; e++) {
+            SDL_SetRenderDrawColor(renderer, matrix[i][e].r, matrix[i][e].g, matrix[i][e].b, matrix[i][e].a);
+            SDL_RenderDrawPoint(renderer, i, e);
+        }
+    }
+    SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window * window = initSDL();
+    SDL_Color ** _2DMatrix = init2DSpace();
+
+    RenderMatrix(_2DMatrix);
+
+    // A partir d'ici le travail se fera sur la matrice et a chaque action d'ecriture on fait un travail de render
+
+
+
+
+    closeEventSDL(_2DMatrix);
     return 0;
 }
