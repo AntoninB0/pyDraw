d217077809dd9f4eb52e95a5331c946b11cb7e40
Antonin <bo.antonin@gmail.com>
2024-09-28 19:32:02 +0200

    add line

--- c/main.c
+++ c/main.c
@@ -1,37 +1,73 @@
 #include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define WIDTH 1920
 #define HEIGHT 1080
-#define DEFAULTCOLOR_R 0
-#define DEFAULTCOLOR_G 255
-#define DEFAULTCOLOR_B 0
-#define DEFAULTCOLOR_A 255
 
+#define DEFAULTCOLOR_BG_R 0
+#define DEFAULTCOLOR_BG_G 255
+#define DEFAULTCOLOR_BG_B 0
+#define DEFAULTCOLOR_BG_A 255
 
-typedef struct PEN{
-    int size,penDown;
-    float x,y,angle;
+#define DEFAULTCOLOR_PEN_R 255
+#define DEFAULTCOLOR_PEN_G 0
+#define DEFAULTCOLOR_PEN_B 0
+#define DEFAULTCOLOR_PEN_A 255
+
+typedef struct PEN {
+    int size, penDown;
+    float x, y, angle;
     SDL_Color color;
-}PEN;
+} PEN;
+
+typedef struct POS {
+    int x, y;
+} POS;
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+SDL_Color **matrix;
 
-SDL_Window * initSDL(){
+void initMatrix() {
+    matrix = malloc(sizeof(SDL_Color*) * WIDTH);
+    if (matrix == NULL) exit(1);
+
+    for (int i = 0; i < WIDTH; i++) {
+        matrix[i] = malloc(sizeof(SDL_Color) * HEIGHT);
+        if (matrix[i] == NULL) exit(1);
+
+        for (int e = 0; e < HEIGHT; e++) {
+            matrix[i][e].r = DEFAULTCOLOR_BG_R;
+            matrix[i][e].g = DEFAULTCOLOR_BG_G;
+            matrix[i][e].b = DEFAULTCOLOR_BG_B;
+            matrix[i][e].a = DEFAULTCOLOR_BG_A;
+        }
+    }
+}
+
+void RenderMatrix() {
+    for (int i = 0; i < WIDTH; i++) {
+        for (int e = 0; e < HEIGHT; e++) {
+            SDL_SetRenderDrawColor(renderer, matrix[i][e].r, matrix[i][e].g, matrix[i][e].b, matrix[i][e].a);
+            SDL_RenderDrawPoint(renderer, i, e);
+        }
+    }
+    SDL_RenderPresent(renderer);
+}
+
+SDL_Window * initSDL() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("Erreur d'initialisation de SDL: %s\n", SDL_GetError());
         exit(1);
     }
 
-    SDL_Window * window = SDL_CreateWindow("Fenêtre SDL2",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          WIDTH, HEIGHT,
-                                          SDL_WINDOW_SHOWN);
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    window = SDL_CreateWindow("Fenêtre SDL2",
+                              SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED,
+                              WIDTH, HEIGHT,
+                              SDL_WINDOW_SHOWN);
 
     if (!window) {
         printf("Erreur de création de la fenêtre: %s\n", SDL_GetError());
@@ -39,10 +75,19 @@ SDL_Window * initSDL(){
         exit(1);
     }
 
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        printf("Erreur de création du renderer: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        exit(1);
+    }
+
+    RenderMatrix();
     return window;
 }
 
-void closeEventSDL(SDL_Color ** matrix){
+void closeEventSDL() {
     SDL_Event e;
     int quit = 0;
     while (!quit) {
@@ -53,7 +98,7 @@ void closeEventSDL(SDL_Color ** matrix){
         }
     }
 
-    // Nettoyer et quitter SDL
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
@@ -63,47 +108,79 @@ void closeEventSDL(SDL_Color ** matrix){
     free(matrix);
 }
 
-SDL_Color ** init2DSpace() {
-    SDL_Color **matrix = malloc(sizeof(SDL_Color*) * WIDTH);
-    if (matrix == NULL) exit(1);
-
-    for (int i = 0; i < WIDTH; i++) {
-        matrix[i] = malloc(sizeof(SDL_Color) * HEIGHT);
-        if (matrix[i] == NULL) exit(1);
-
-        for (int e = 0; e < HEIGHT; e++) {
-            matrix[i][e].r = DEFAULTCOLOR_R;
-            matrix[i][e].g = DEFAULTCOLOR_G;
-            matrix[i][e].b = DEFAULTCOLOR_B;
-            matrix[i][e].a = DEFAULTCOLOR_A;
-        }
-    }
-
-    return matrix;
+int approxPosX(float x) {
+    return (x - (int)x > 0.5) ? (int)x + 1 : (int)x;
 }
 
-void RenderMatrix(SDL_Color** matrix) {
-    for (int i = 0; i < WIDTH; i++) {
-        for (int e = 0; e < HEIGHT; e++) {
-            SDL_SetRenderDrawColor(renderer, matrix[i][e].r, matrix[i][e].g, matrix[i][e].b, matrix[i][e].a);
-            SDL_RenderDrawPoint(renderer, i, e);
+int approxPosY(float y) {
+    return (y - (int)y > 0.5) ? (int)y + 1 : (int)y;
+}
+
+POS approxPos(float x, float y) {
+    POS coord;
+    coord.x = approxPosX(x);
+    coord.y = approxPosY(y);
+    return coord;
+}
+
+PEN initPen() {
+    PEN pen;
+    pen.size = 1;
+    pen.penDown = 0;
+    pen.x = (float)(WIDTH / 2);
+    pen.y = (float)(HEIGHT / 2);
+    pen.angle = 0;
+    pen.color.r = DEFAULTCOLOR_PEN_R;
+    pen.color.g = DEFAULTCOLOR_PEN_G;
+    pen.color.b = DEFAULTCOLOR_PEN_B;
+    pen.color.a = DEFAULTCOLOR_PEN_A;
+    return pen;
+}
+
+float float2Rad(float degrees) {
+    return degrees * (M_PI / 180.0f);
+}
+
+PEN lineWrite(int length, PEN pen) {
+    for (int i = 0; i < length; i++) {
+        float newX = pen.x + cosf(float2Rad(pen.angle));
+        float newY = pen.y - sinf(float2Rad(pen.angle));
+
+        int x = approxPosX(newX);
+        int y = approxPosY(newY);
+
+        if (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+            matrix[x][y] = pen.color;
         }
+
+        pen.x = newX;
+        pen.y = newY;
     }
-    SDL_RenderPresent(renderer);
+
+    RenderMatrix();
+    return pen;
 }
 
 int main(int argc, char* argv[]) {
+    initMatrix();
+    initSDL();
 
-    SDL_Window * window = initSDL();
-    SDL_Color ** _2DMatrix = init2DSpace();
+    PEN pen = initPen();
 
-    RenderMatrix(_2DMatrix);
+    pen.angle = 50;
+    pen = lineWrite(100, pen);
+    printf("Position after first line: %f, %f\n", pen.x, pen.y);
 
-    // A partir d'ici le travail se fera sur la matrice et a chaque action d'ecriture on fait un travail de render
+    pen.angle += 70;
+    pen = lineWrite(100, pen);
 
+    for (int i = 0; i < 10 ; ++i) {
+        pen.angle += 70;
+        pen = lineWrite(500, pen);
+    }
 
+    printf("Position after second line: %f, %f\n", pen.x, pen.y);
 
-
-    closeEventSDL(_2DMatrix);
+    closeEventSDL();
     return 0;
 }
